a45e34d850bd27193d23d2422bd8352cd91f19df
Anton <anton@Anton-MacBook-Pro.local>
2023-04-22 19:44:45 +0600

    initial commit

---
+++ git_commands.py
@@ -0,0 +1,14 @@
+"""
+git init - 
+git remote add name url - добавляет удаленный репозиторий
+git pull origin master - стягивает изменения с проекта
+git status
+git add
+git commit -m "комментарий должен описывать изменения"
+git branch - менеджер веток
+git branch name branch - создание новой ветки
+git checkout name branch
+git push origin master
+git reset filename
+
+"""
